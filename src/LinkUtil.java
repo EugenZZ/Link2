f1b08d7e90c364bad56fba9ee9f699789b481172
Erying <1845475789@qq.com>
2023-03-19 12:54:11 +0800

    Revert "Link4"
    This reverts commit 9e857de6

--- src/LinkUtil.java
+++ src/LinkUtil.java
@@ -1,10 +1,10 @@
 public class LinkUtil {
    public static boolean isRound(Node head) {
-       if (head.next == null || head.next.next == null) {
+       if (head == null || head.next == null) {
            return false;
        }
-       Node slow=head.next;
-       Node fast=head.next.next;
+       Node slow=head;
+       Node fast=head.next;
        while (fast!=slow) {
            //当走到null代表为非循环链表结束
            if (fast.next == null || fast.next.next == null) {
--- src/Node.java
+++ src/Node.java
@@ -2,9 +2,7 @@ public class Node {
     public Node pre;
     public Node next;
     public String data;
-   public Node(){
 
-   }
     public Node(String data) {
         this.data = data;
     }
